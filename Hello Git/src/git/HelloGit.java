97a5f3f1b73d9c186ab1ddaa86602b32e095b16e
luissemxoro <91712603+luissemxoro@users.noreply.github.com>
2021-10-06 16:49:34 -0300

    Add files via upload

---
+++ Hello Git/src/git/HelloGit.java
@@ -0,0 +1,14 @@
+package git;
+
+import javax.swing.JOptionPane;
+
+public  class  HelloGit {
+
+	public  static  void  main ( String [] args ) {
+		
+			System.out.println ( " Olá Git " );
+			int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a Idade do usuario"));
+			System.out.println("Idade do Usuario " +idade);
+	}
+
+}
